797635ce36e2e4ba559615e5ba93ebf0a1bfc06f
alinemati45 <nemati@uwm.edu>
2025-05-03 04:48:21 -0500

    lintf fix error

--- app/app/test.py
+++ app/app/test.py
@@ -5,10 +5,10 @@ Sample test file for the app module.
 from django.test import SimpleTestCase
 from app import calc
 
+
 class ClacTests(SimpleTestCase):
     """Test the calc module."""
     def test_add(self):
         """Test the add function."""
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
-        
