f0c97e95ce682d3739d01f5ea11c86315ef6db19
rezashakeri45 <nemati@uwm.edu>
2025-05-13 02:20:09 -0500

    update

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,31 @@
-from django.contrib import admin # noqa
+"""
+Django admin customization for the app.
 
-# Register your models here.
+"""
+
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+
+from core import models
+
+
+class UserAdmin(BaseUserAdmin):
+    """Define admin model for custom User model with no email field."""
+
+    ordering = ["id"]
+    list_display = ["email", "name"]
+    list_filter = ["is_active", "is_staff"]
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_active", "is_staff")}),
+        ("Personal", {"fields": ("name",)}),
+    )
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2"),
+        }),
+    )
+
+admin.site.register(models.User, UserAdmin) 
